549a89e8e00a20501508f0beb4cfd4f976cffb0a
Manju M Johny <mjohny@iastate.edu>
2023-11-15 16:09:16 -0800

    update fanova code

--- code/fanova_analysis.R
+++ code/fanova_analysis.R
@@ -41,6 +41,7 @@ df<-sifdat
 fireday<-paste("2018-07-27")
 
 mytheme=theme_light()+theme(axis.text.x=element_text(angle=45, hjust=1, vjust=1))
+mytheme2=theme_classic()+theme(axis.text.x=element_text(angle=45, hjust=1, vjust=1))
 
 #Raw SIF (original sif time series ) with dashed line representing fire
 Raw_sif_plot<-ggplot(data=df, aes(x=date, y=sif, group=lonlat, color = group), alpha=0.7)+
@@ -141,7 +142,7 @@ for (i in 1:nrow(fullseriesU)){
   fdpar = fdPar(basis, Lfdobj=int2Lfd(2), lambda=optlambda)
   smooth<-smooth.basis(argvals=new_time, y = new_series, wtvec=wt, fdParobj=fdpar)
   fullseriesU[i,]<-fdata(smooth$fd, argvals=argvals)$data #save the re-discretized series post-smoothing 
-  }
+}
 
 for (i in 1:nrow(fullseriesB)){
   new_time<-dfB_curves[[i]]$dayssinceapril1 #time
@@ -153,6 +154,7 @@ for (i in 1:nrow(fullseriesB)){
   fullseriesB[i,]<-fdata(smooth$fd, argvals=argvals)$data #save the re-discretized series post-smoothing 
 }
 
+#create combined long for data set for plotting
 smoothdfU<-data.frame(argvals=argvals, smoothU = t(fullseriesU), meanUnburned=rowMeans(t(fullseriesU)))
 smoothdfU_l<-smoothdfU %>% pivot_longer(-argvals, names_to = "variable")
 smoothdfU_l$group<-"unburned"
@@ -163,28 +165,24 @@ smoothdfB_l$group<-"burned"
 smoothdf<-rbind(smoothdfU_l, smoothdfB_l)
 head(smoothdf)
 
-p3<-ggplot(data=smoothdf, aes(x=argvals, y=value, group=variable, col=group))+geom_line()+scale_color_manual(labels = c("burned", "unburned"), values=c('darkgrey','lightgreen'))+
+#plot smooth curves along with smooth mean curves of each group
+smoothplot1<-ggplot(data=smoothdf, aes(x=argvals, y=value, group=variable, col=group))+geom_line()+scale_color_manual(labels = c("burned", "unburned"), values=c('darkgrey','lightgreen'))+
   geom_line(data=subset(smoothdf, variable == "meanUnburned" | variable == "meanBurned"), aes(x=argvals, y=value, lty = variable), col="black")+
   labs(title="Smooth SIF Curves", x="Days Since April 1, 2018", y="SIF", col="Group", lty="Mean")+mytheme
+ggplotly(smoothplot1)
 
-ggplotly(p3)
-
-
-
-
-smooth_plot<-ggplot(data=smoothdf, aes(x=argvals, y=value, group=variable, col=group))+geom_line()+scale_color_manual(labels = c("burned", "unburned"), values=c('black','lightgreen'))+
+#smooth curves with no means
+smooth_plot2<-ggplot(data=smoothdf, aes(x=argvals, y=value, group=variable, col=group))+geom_line()+scale_color_manual(labels = c("burned", "unburned"), values=c('black','lightgreen'))+
   labs(title="Smooth Curves", x="Days Since April 1, 2018", y="SIF", col="Group", lty="Mean")+mytheme2+ylim(-0.5,1.5)
 
-
-
+#smooth means plot
 meanplot<-subset(smoothdf, variable == "meanUnburned" | variable == "meanBurned")
 meanplot$variable[meanplot$variable=="meanUnburned"]<-"unburned"
 meanplot$variable[meanplot$variable=="meanBurned"]<-"burned"
 
-smooth_plot<-ggplot()+
+smooth_mean_plot<-ggplot()+
   geom_line(data=meanplot, aes(x=argvals, y=value, lty = variable), col="black")+
   geom_line()+labs(title="Mean Smooth Curves", x="Days Since April 1, 2018", y="SIF", lty="Mean")+mytheme2+ylim(-0.5,1.5)
-
 
 
 
@@ -197,14 +195,15 @@ fdpar = fdPar(basis, Lfdobj=int2Lfd(2))
 
 smooth1<-smooth.basis(argvals = argvals, y=t(fullseriesU), fdParobj=fdpar)
 smooth2<-smooth.basis(argvals = argvals, y=t(fullseriesB), fdParobj=fdpar)
-plot(smooth2)
+plot(smooth2) #plot smooth objects
 
+#calculate test statistic
 smean1<-mean.fd(smooth1$fd)
 smean2<-mean.fd(smooth2$fd)
 stat<-metric.lp(fdata(smean1, argvals=argvals), fdata(smean2, argvals=argvals))[1] #1.97
-stat #7.056177
+stat #7.056177 
 
-# represent each curves as pca 
+# represent each curves by fpca (functional principal component analysis) 
 pcalist1 = pca.fd(smooth1$fd, 4, centerfns=TRUE)
 pcalist2 = pca.fd(smooth2$fd, 4, centerfns=TRUE)
 #plot.pca.fd(pcalist1)
@@ -354,6 +353,7 @@ fun_reconstruct2 <- function(x) {
 # Generate resamples 
 ptm_start <- proc.time()
 for (j in 1:N){
+  #create spatial model with desired parameter values
   scoremodel1_1 <- RMtrend(mean=0)+RMexp(var=v1_1, scale = s1_1)
   scoremodel1_2 <- RMtrend(mean=0)+RMexp(var=v1_2, scale = s1_2)
   scoremodel1_3 <- RMtrend(mean=0)+RMexp(var=v1_3, scale = s1_3)
@@ -364,6 +364,7 @@ for (j in 1:N){
   scoremodel2_3 <- RMtrend(mean=0)+RMexp(var=v2_3, scale = s2_3)
   scoremodel2_4 <- RMtrend(mean=0)+RMexp(var=v2_4, scale = s2_4)
   
+  #simulate spatial field of scores for 4 principal components for each group
   z1_1 <- RFsimulate(scoremodel1_1, x=x_coord1, y=y_coord1)$variable1
   z1_2 <- RFsimulate(scoremodel1_2, x=x_coord1, y=y_coord1)$variable1
   z1_3 <- RFsimulate(scoremodel1_3, x=x_coord1, y=y_coord1)$variable1
@@ -374,20 +375,27 @@ for (j in 1:N){
   z2_3 <- RFsimulate(scoremodel2_3, x=x_coord2, y=y_coord2)$variable1
   z2_4 <- RFsimulate(scoremodel2_4, x=x_coord2, y=y_coord2)$variable1
   
+  #data of generated scores
   scores_tilde1<-cbind(z1_1,z1_2,z1_3,z1_4)
   scores_tilde2<-cbind(z2_1,z2_2,z2_3,z2_4)
   
+  #construct resample curves from the fpcs
   pcres1<-apply(scores_tilde1, 1,FUN=fun_reconstruct1)
   pcres2<-apply(scores_tilde2, 1,FUN=fun_reconstruct2)
   
+  #obtain mean resample curve for each group and save
   dep_pcres_mean1[,j]<-mean(fdata(t(pcres1)))$data
   dep_pcres_mean2[,j]<-mean(fdata(t(pcres2)))$data
+  
+  #obtain and save monte carlo statistic (distance between mean resample curve for each group)
   dist[j] <-metric.lp(fdata(mean(fdata(t(pcres1)))$data, argvals=argvals), 
                       fdata(mean(fdata(t(pcres2)))$data, argvals=argvals))[1] 
+  
+  #print itteration to keep track of progress
   print(j)
 }
 
-ptm <- proc.time()-ptm_start
+ptm <- proc.time()-ptm_start #time to generate resamples (typically a few mins)
 
 #calculate p-values 
 pval1<-length(dist[dist>stat])/length(dist) 
@@ -400,27 +408,31 @@ resdflong<-resdf %>% pivot_longer(-argvals, names_to = "variable")
 mytheme=theme_light()+theme(axis.text.x=element_text(angle=45, hjust=1, vjust=1))
 mytheme2=theme_classic()+theme(axis.text.x=element_text(angle=45, hjust=1, vjust=1))
 
-fanova<-ggplot()+geom_line(data=resdflong, aes(x=as_date(argvals, origin = "2018-04-01"), y=value, group=variable), colour="darkgray", alpha = 0.2)+
+#fanova visualization with p-value annotated
+fanova1<-ggplot()+geom_line(data=resdflong, aes(x=as_date(argvals, origin = "2018-04-01"), y=value, group=variable), colour="darkgray", alpha = 0.2)+
   geom_line(data=subset(resdflong, variable == "res.1"|variable == "sample"),aes(x=as_date(argvals, origin = "2018-04-01"),y=value, col=variable))+
   scale_color_manual(labels = c("Resamples", "Sample"), values=c("darkgray",'black'))+
   theme(legend.title = element_blank())+geom_vline(xintercept = as.Date(fireday), linetype = "dashed")+
   labs(title="fANOVA Visualization (Burned - Unburned)",x= "time", y = "Mean Difference", col="")+mytheme2+scale_x_date(date_breaks="2 month")+
   annotate(geom="text", x=as_date(argvals[1])+Inf, y=Inf,label=paste("global p-value =",pval1),hjust = 1, vjust = 1)+ylim(-0.5,0.5)
 
-fanova<-ggplot()+geom_line(data=resdflong, aes(x=as_date(argvals, origin = "2018-04-01"), y=value, group=variable), colour="darkgray", alpha = 0.2)+
+#fanova visualization no p-value annotated
+fanova2<-ggplot()+geom_line(data=resdflong, aes(x=as_date(argvals, origin = "2018-04-01"), y=value, group=variable), colour="darkgray", alpha = 0.2)+
   geom_line(data=subset(resdflong, variable == "res.1"|variable == "sample"),aes(x=as_date(argvals, origin = "2018-04-01"),y=value, col=variable))+
   scale_color_manual(labels = c("Resamples", "Sample"), values=c("darkgray",'black'))+
   theme(legend.title = element_blank())+geom_vline(xintercept = as.Date(fireday), linetype = "dashed")+
   labs(title="fANOVA Visualization (Burned - Unburned)",x= "time", y = "Mean Difference", col="")+mytheme2+scale_x_date(date_breaks="2 month")+
   ylim(-0.5,0.5)
 
-fanova2<-ggplot()+geom_line(data=resdflong, aes(x=argvals, y=value, group=variable), colour="darkgray", alpha = 0.2)+
+#fanova visualization day axis
+fanova3<-ggplot()+geom_line(data=resdflong, aes(x=argvals, y=value, group=variable), colour="darkgray", alpha = 0.2)+
   geom_line(data=subset(resdflong, variable == "res.1"|variable == "sample"),aes(x=argvals,y=value, col=variable))+
   scale_color_manual(labels = c("Resamples", "Sample"), values=c("darkgray",'black'))+
   theme(legend.title = element_blank())+
   labs(title="fANOVA Visualization (Burned - Unburned)",x= "Days since April 1, 2018", y = "Mean Difference", col="")+mytheme2+
   annotate(geom="text", x=Inf, y=Inf,label=paste("global p-value =",pval1),hjust = 1, vjust = 1)+ylim(-0.5,0.5)
 
+#sample difference curve only 
 diff_curve<-ggplot()+
   geom_line(data=subset(resdflong, variable == "sample"),aes(x=argvals,y=value, col=variable))+
   scale_color_manual(labels = c("Sample"), values=c('black'))+
@@ -428,12 +440,14 @@ diff_curve<-ggplot()+
   labs(title="Sample Difference (Burned - Unburned)",x= "Days since April 1, 2018", y = "Mean Difference", col="")+mytheme2+
   ylim(-0.5,0.5)
 
-
+# create histogram of bootstrap distances to visualize p-value calculation
 d<-data.frame(dist=dist)
 head(d)
 
+# Montecarlo statistic distribution with test statistic (dashed line)
 hist1<-ggplot(data=d, aes(x=dist))+geom_histogram(col="black",fill="white")+mytheme2+geom_vline(xintercept = stat, linetype=2)+xlim(0,8.5)+
   annotate(geom="text", x=Inf, y=Inf,label=paste("global p-value =",pval1),hjust = 1, vjust = 1)+labs(y="Count", x="Distance", title = "Bootstrap Distances")
 
+#test statistic only 
 hist2<-ggplot(data=d, aes(x=dist))+geom_histogram(col="white",fill="white")+mytheme2+geom_vline(xintercept = stat, linetype=2)+xlim(0,8.5)+
   annotate(geom="text", x=Inf, y=Inf,label=paste("Test Statistic"),hjust = 1, vjust = 1)+labs(y="Count", x="Distance", title = "Bootstrap Distances")
